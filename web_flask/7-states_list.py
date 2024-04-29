0b1a6b6f901f0d5ca5e866aa67d11c5d2ef4f082
Ndikubwimana Balthazar <nbarthazar82@gmail.com>
2024-04-29 08:27:01 -0700

    RESTful

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,7 +19,7 @@ def states_list():
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
-storage.close()
+    storage.close()
 
 
 if __name__ == '__main__':
